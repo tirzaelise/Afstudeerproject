e0b235bd5b29b89088af8e8bec7c3b4ae8879f73
tirzaelise <tirza.soute@student.uva.nl>
2017-05-16 13:02:56 +0200

    comments updates

--- python/create_database.py
+++ python/create_database.py
@@ -3,7 +3,7 @@
 # This file creates the drinks database using the Absolut Drinks Database. It
 # retrieves all the entries that are in the database and saves them in a
 # Pickle file in a Dictionary using the following format:
-# {drink name: drink object}.
+# {drink name: list of drink properties}.
 
 import drink
 import json
@@ -12,8 +12,9 @@ import cPickle as pickle
 import urllib2
 
 
-# Scrapes the web page of the Absolut Drinks Database.
 def scrape_page(next_url, first_scrape):
+    """ Scrapes a web page of the Absolut Drinks Database. """
+
     url_to_api = "http://addb.absolutdrinks.com"
     api_key = "/?apiKey=4093c8e39ec5436391012017bc354c01"
     url = url_to_api + "/" + "drinks" + api_key
@@ -26,8 +27,11 @@ def scrape_page(next_url, first_scrape):
     return json.load(urllib2.urlopen(url))
 
 
-# Creates the database and returns the next url tp update the database.
 def create_database(data):
+    """
+    Creates the database and returns the next url to update the database.
+    """
+
     update_database(data, drinks_dict)
     next_url = data.get("next")
 
@@ -37,20 +41,19 @@ def create_database(data):
     return next_url
 
 
-# Updates the database (Dictionary) of drinks.
 def update_database(data, drinks_dict):
+    """ Updates the database (Dictionary) of drinks. """
+
     for element in data.get("result"):
         info = get_info(element)
-        # make_drink = getattr(drink, "create_drink")
-        # drink_object = make_drink(info[0], info[1], info[2], info[3], info[4],
-        #                           info[5], info[6], info[7], info[8], info[9],
-        #                           info[10], info[11])
-        # drinks_dict.update({drink_object.name.lower(): drink_object})
         drinks_dict.update({info[0]: info})
 
 
-# Retrieves the requested properties of a drink and returns them in a list.
 def get_info(drink):
+    """
+    Retrieves the requested properties of a drink and returns them in a list.
+    """
+
     properties = ["name", "descriptionPlain", "color", "skill", "isAlcoholic",
                   "isCarbonated", "isHot", "ingredients", "tastes", "occasions",
                   "tools", "actions"]
@@ -73,9 +76,12 @@ def get_info(drink):
     return info
 
 
-# Converts a JSON array into a string that only contains the text values in
-# lower case.
 def json_to_array(json_array):
+    """
+    Converts a JSON array into a string that only contains the text values in
+    lower case.
+    """
+
     array_string = ""
 
     for element in json_array:
@@ -90,9 +96,12 @@ def json_to_array(json_array):
     return array_string
 
 
-# Converts the value of a boolean to a string that holds the information, such
-# that "is_carbonated(True)" becomes "carbonated".
 def bool_to_str(drink_property, property_value):
+    """
+    Converts the value of a boolean to a string that holds the information, such
+    that "is_carbonated(True)" becomes "carbonated".
+    """
+
     string = ""
 
     if drink_property == "isAlcoholic":
@@ -113,9 +122,9 @@ def bool_to_str(drink_property, property_value):
     return string
 
 
-# Saves the database to a Pickle file.
 def save_database(database):
-    # if not os.path.exists("database.pkl"):
+    """ Saves the database to a Pickle file. """
+
     pickle.dump(database, open("database.pkl", "wb"))
 
 
--- python/create_keywords.py
+++ python/create_keywords.py
@@ -1,3 +1,8 @@
+# This file creates the list of key words using the database that was created
+# from the Absolut Drinks Database. All the information that is in the database
+# is added to a list and this list is cleaned by flattening it and removing
+# duplicates.
+
 # !/usr/bin/env python2
 
 import collections
@@ -6,16 +11,20 @@ import pickle
 from PyDictionary import PyDictionary
 
 
-# Loads the database.
 def load_database():
+    """ Loads the database. """
+
     if os.path.exists("database.pkl"):
         return pickle.load(open("database.pkl", "rb"))
 
 
-# Creates the list of key words using drinks database using their name,
-# description, color, required skill level, ingredients, tastes, occasions,
-# tools and actions.
 def make_keywords():
+    """
+    Creates the list of key words using drinks database using their name,
+    description, color, required skill level, ingredients, tastes, occasions,
+    tools and actions.
+    """
+
     key_words = ["alcoholic", "description", "carbonated", "hot", "cold",
                  "skill", "color", "tastes", "occasions", "tools", "actions"]
 
@@ -38,9 +47,12 @@ def make_keywords():
     return key_words
 
 
-# Use the words in a string to create a list that holds each word as an
-# element.
 def split_string(string):
+    """
+    Use the words in a string to create a list that holds each word as an
+    element.
+    """
+
     string_list = []
 
     for word in string.split(" "):
@@ -48,16 +60,12 @@ def split_string(string):
     return string_list
 
 
-# Flattens the list of key words, removes duplicates and removes None types.
-def clean_keywords(key_words):
-    flattened_list = list(flatten(key_words))
-    key_words = remove_duplicates(flattened_list)
-    return [key_word for key_word in key_words if key_word is not None]
-
-
-# Removes the measurements from the ingredients such that "1/2 part lime"
-# becomes "lime".
 def only_ingredients(ingredients):
+    """
+    Removes the measurements from the ingredients such that "1/2 part lime"
+    becomes "lime".
+    """
+
     new_ingredients = []
 
     for ingredient in ingredients:
@@ -66,11 +74,14 @@ def only_ingredients(ingredients):
     return new_ingredients
 
 
-# Removes the measurements from one ingredient. The words in an ingredient are
-# always separated by a space. This is done by checking if the first letter of
-# a word is a letter. If it is not, the first two elements in the ingredient are
-# deleted.
 def remove_measurements(string):
+    """
+    Removes the measurements from one ingredient. The words in an ingredient are
+    always separated by a space. This is done by checking if the first letter of
+    a word is a letter. If it is not, the first two elements in the ingredient
+    are deleted.
+    """
+
     split_string = string.split(" ")
 
     if not split_string[0].isalpha():
@@ -79,8 +90,19 @@ def remove_measurements(string):
     return split_string
 
 
-# Flattens a list.
+def clean_keywords(key_words):
+    """
+    Flattens the list of key words, removes duplicates and removes None types.
+    """
+
+    flattened_list = list(flatten(key_words))
+    key_words = remove_duplicates(flattened_list)
+    return [key_word for key_word in key_words if key_word is not None]
+
+
 def flatten(unflattened_list):
+    """ Flattens a list. """
+
     for element in unflattened_list:
         if isinstance(element, collections.Iterable) and not \
            isinstance(element, basestring):
@@ -90,14 +112,17 @@ def flatten(unflattened_list):
             yield element
 
 
-# Removes the duplicates from a list.
 def remove_duplicates(key_words):
+    """ Removes the duplicates from a list. """
     return list(set(key_words))
 
 
-# Returns all the synonyms of the key words and saves them in a list, including
-# the original key words. The synonyms are obtained using PyDictionary.
 def get_synonyms(key_words):
+    """
+    Returns all the synonyms of the key words and saves them in a list, including
+    the original key words. The synonyms are obtained using PyDictionary.
+    """
+
     dictionary = PyDictionary()
     final_keywords = []
 
@@ -113,10 +138,10 @@ def get_synonyms(key_words):
     return final_keywords
 
 
-# Saves the key words in a Pickle file.
 def save_keywords(key_words):
-    if not os.path.exists("key_words.pkl"):
-        pickle.dump(key_words, open("key_words.pkl", "wb"))
+    """ Saves the key words in a Pickle file. """
+
+    pickle.dump(key_words, open("key_words.pkl", "wb"))
 
 
 if __name__ == "__main__":
