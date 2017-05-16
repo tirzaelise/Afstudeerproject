f4bc627a63975b0af3a5261e89efba3424b41a91
tirzaelise <tirza.soute@student.uva.nl>
2017-05-16 15:14:35 +0200

    code to ask for clarification before updating the list of available drinks

--- python/understand_language.py
+++ python/understand_language.py
@@ -9,14 +9,16 @@ from pprint import pprint
 import time
 
 
-# Loads the database of drinks, which is a Python Dictionary.
 def load_database():
+    """ Loads the database of drinks, which is a Python Dictionary. """
+
     if os.path.exists("database.pkl"):
         return pickle.load(open("database.pkl", "rb"))
 
 
-# Loads the Stanford Dependency Parser to parse sentences.
 def load_parser():
+    """ Loads the Stanford Dependency Parser to parse sentences. """
+
     jar_path = "stanford-parser-full-2016-10-31/stanford-parser.jar"
     models_path = "stanford-parser-full-2016-10-31/stanford-parser-" + \
         "3.7.0-models.jar"
@@ -24,22 +26,27 @@ def load_parser():
                                     path_to_models_jar=models_path)
 
 
-# Loads the key words that will be checked for occurrences of words in a natural
-# sentence.
 def load_keywords():
+    """
+    Loads the key words that will be checked for occurrences of words in a
+    natural sentence.
+    """
+
     if os.path.exists("key_words.pkl"):
         return pickle.load(open("key_words.pkl", "rb"))
 
 
-# Uses the Stanford Dependency Parser to parse a sentence.
 def parse_sentence(parser, sentence):
+    """ Uses the Stanford Dependency Parser to parse a sentence. """
+
     result = parser.raw_parse(sentence)
     dep = result.next()
     return list(dep.triples())
 
 
-# Returns all the verbs in a sentence.
 def get_verbs(sentence):
+    """ Returns all the verbs in a sentence. """
+
     verbs = []
     tokenized_sentence = word_tokenize(sentence)
     tagged_sentence = pos_tag(tokenized_sentence)
@@ -50,8 +57,9 @@ def get_verbs(sentence):
     return verbs
 
 
-# Analyses a sentence: returns the subject, verb and object.
 def analyse_sentence(verbs, sentence):
+    """ Analyses a sentence: returns the subject, verb and object. """
+
     subjects = []
     objects = []
     negations = []
@@ -65,8 +73,9 @@ def analyse_sentence(verbs, sentence):
     return verbs, subjects, objects, negations
 
 
-# Returns the word that has the requested function in a sentence.
 def get_function_word(sentence, verb, requested_function):
+    """ Returns the word that has the requested function in a sentence. """
+
     function_word = None
 
     for word in sentence:
@@ -78,10 +87,11 @@ def get_function_word(sentence, verb, requested_function):
     return function_word
 
 
-# Sometimes a verb, that does not have a subject, is found. A sentence needs to
-# have both a subject and a verb in order to form a sentence so then that verb
-# can be omitted.
 def correct_functions(verbs, subjects, objects, negations):
+    """
+    Corrects sentences such that verbs that do not have a subject are omitted.
+    """
+
     i = 0
 
     for subject in subjects:
@@ -94,25 +104,29 @@ def correct_functions(verbs, subjects, objects, negations):
     return verbs, subjects, objects, negations
 
 
-# Checks if the found verbs, subjects and objects are in the key words. If they
-# are, the list of ordered drinks is updated with the new information. If they
-# aren't, the robot asks for clarification.
-def do_action(verbs, subjects, objects, negations, key_words, ordered_drinks):
-    for i in range(0, len(verbs)):
-        unknown_words = check_keywords(verbs[i], subjects[i], objects[i],
-                                       key_words)
-        # if len(unknown_words) != 0:
-        #     clarification = ask_clarification(unknown_words)
-        #     print clarification
-        # else:
-        update_database(verbs[i], subjects[i], objects[i], negations[i],
-                        ordered_drinks)
+def ask_clarification(verb, subject, s_object, negation, key_words):
+    """
+    Checks if the found verbs, subjects and objects are in the key words. If
+    they are, the list of ordered drinks is updated with the new information. If
+    they aren't, the robot asks for clarification.
+    """
+
+    unknown_words = check_keywords(verb, subject, s_object, key_words)
+    # if len(unknown_words) != 0:
+    #     clarification = misunderstood(unknown_words)
+    # else:
+    drink_property = find_drink_property(s_object)
+    clarification = understood(s_object, drink_property, negation)
+    return clarification
 
 
-# Checks if the found verb, subject and object are in the list of key words.
-# Returns a list that contains the words that were not found in the key
-# words.
 def check_keywords(verb, subject, s_object, key_words):
+    """
+    Checks if the found verb, subject and object are in the list of key words.
+    Returns a list that contains the words that were not found in the key
+    words.
+    """
+
     unknown_words = []
 
     if verb:
@@ -127,11 +141,15 @@ def check_keywords(verb, subject, s_object, key_words):
     return unknown_words
 
 
-# Returns a sentence that asks for clarification given the words in the
-# sentence that were not understood, because they are not in the list of key
-# words.
-def ask_clarification(unknown_words):
+def misunderstood(unknown_words):
+    """
+    Returns a sentence that asks for clarification given the words in the
+    sentence that were not understood, because they are not in the list of key
+    words.
+    """
+
     variable_words = ", ".join(unknown_words)
+    request = " Please describe your response differently."
 
     if len(unknown_words) > 1:
         k = variable_words.rfind(", ")
@@ -139,44 +157,37 @@ def ask_clarification(unknown_words):
         clarification = "I did not understand the words " + variable_words + "."
     else:
         clarification = "I did not understand the word " + variable_words + "."
-    return clarification
+    return clarification + request
 
 
-# Uses the verb, subject, object and optional negation that were obtained from
-# a natural language sentence to update the list of ordered drinks.
-def update_database(verb, subject, s_object, negation, ordered_drinks):
-    for drink in ordered_drinks:
-        drink_properties = database.get(drink)
+def find_drink_property(word):
+    """
+    Finds the drink property that has the shortest distance to a common hypernym
+    between a drink property and a word.
+    """
 
-        # if substring_in_list(s_object, drink_properties):
-        find_closest_hypernym(s_object, drink_properties)
-
-
-# Returns a boolean that indicates whether a substring can be found in a list.
-def substring_in_list(substring, l_ist):
-    new_list = [s for s in l_ist if substring in s]
-    if len(new_list) > 0:
-        return True
-    return False
-
-
-# Finds the drink property that has the shortest distance to a common hypernym
-# between a drink property and a word.
-def find_closest_hypernym(word, drink_properties):
+    properties = ["drink", "color", "skill", "alcoholic", "non-alcoholic",
+                  "carbonated", "non-carbonated", "hot", "cold", "ingredient",
+                  "taste", "occasion", "tool", "action"]
     shortest_distance = 1000
 
-    for drink_property in drink_properties:
+    for drink_property in properties:
         property_synsets = wn.synsets(drink_property)
         word_synsets = wn.synsets(word)
         property_syn, word_syn, distance = find_best_synset(property_synsets,
                                                             word_synsets)
-        if distance < shortest_distance:
+        if distance < shortest_distance and word in str(word_syn):
             shortest_distance = distance
             best_property = drink_property
-    print shortest_distance, best_property
+    return best_property
 
 
 def find_best_synset(synsets_1, synsets_2):
+    """
+    Uses the shortest distance to a common hypernym to find the two synsets that
+    are likely the best options for two words.
+    """
+
     shortest_distance = 1000
     best_synset_1 = ""
     best_synset_2 = ""
@@ -193,9 +204,74 @@ def find_best_synset(synsets_1, synsets_2):
     return best_synset_1, best_synset_2, shortest_distance
 
 
+def understood(word, drink_property, negation):
+    """
+    Uses the most similar drink property to generate a response to the natural
+    sentence that was spoken.
+    """
+
+    vowels = ("a", "e", "i", "o", "u")
+
+    if negation:
+        if drink_property.startswith(vowels):
+            response = "I understand you don't have " + word + " as an " + \
+                       drink_property + ". Is this correct?"
+        else:
+            response = "I understand you don't have " + word + " as a " + \
+                       drink_property + ". Is this correct?"
+    else:
+        if drink_property.startswith(vowels):
+            response = "I understand you have " + word + " as an " + \
+                       drink_property + ". Is this correct?"
+        else:
+            response = "I understand you have " + word + " as a " + \
+                       drink_property + ". Is this correct?"
+    return response
+
+
+def speak(string):
+    """ Makes the robot say a string. """
+
+    string
+
+
+def ask_confirmation():
+    """
+    Asks for confirmation from the person the robot is talking to based on the
+    question that was asked and performs an action accordingly.
+    """
+
+    confirmed = True
+    return confirmed
+
+
+def update_drinks(database, drinks, word, negation):
+    """
+    Updates the list of ordered drinks.
+    """
+
+    for drink in ordered_drinks:
+        properties = database.get(drink)
+        if negation and substring_in_list(word, properties):
+            drinks.remove(drink)
+    return drinks
+
+
+def substring_in_list(substring, l_ist):
+    """
+    Returns a boolean that indicates whether a substring can be found in a list.
+    """
+
+    substring_list = [s for s in l_ist if substring in s]
+    if len(substring_list) > 0:
+        return True
+    return False
+
+
 if __name__ == "__main__":
     ordered_drinks = ["martini"]
-    sentence = "I don't have any gin."
+    available_drinks = ordered_drinks
+    sentence = "I can't shake any drinks."
 
     database = load_database()
     parser = load_parser()
@@ -204,4 +280,16 @@ if __name__ == "__main__":
     verbs = get_verbs(sentence)
     verbs, subjects, objects, negations = analyse_sentence(verbs,
                                                            parsed_sentence)
-    do_action(verbs, subjects, objects, negations, key_words, ordered_drinks)
+
+    print verbs, subjects, objects, negations
+
+    # for i in range(0, len(verbs)):
+    #     clarification = ask_clarification(verbs[i], subjects[i], objects[i],
+    #                                       negations[i], key_words)
+    #     speak(clarification)
+    #     confirmed = ask_confirmation()
+    #     if confirmed:
+    #         update_drinks(database, available_drinks, objects[i], negations[i])
+    #     else:
+    #         speak("Please describe what you mean differently.")
+    # print available_drinks
